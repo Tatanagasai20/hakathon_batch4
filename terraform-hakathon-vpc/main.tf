be84e19696bda08fa5eac2e18449aec7af5fa3da
Tatanagasai20 <142307963+Tatanagasai20@users.noreply.github.com>
2025-11-09 14:34:31 +0530

    Change VPC CIDR block and update subnets

--- terraform-hakathon-vpc/main.tf
+++ terraform-hakathon-vpc/main.tf
@@ -1,12 +1,12 @@
 resource "aws_vpc" "main" {
-  cidr_block = "172.10.0.0/16"
+  cidr_block = "172.1.0.0/16"
     tags = {
         Name = "my_vpc"
     }
 }
 resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "172.10.1.0/24"
+  cidr_block        = "172.1.1.0/24"
   availability_zone = "ap-south-1a"
     tags = {
         Name = "public-subnet-1"
@@ -14,7 +14,7 @@ resource "aws_subnet" "public_1" {
 }
 resource "aws_subnet" "public_2" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "172.10.2.0/24"
+  cidr_block        = "172.1.2.0/24"
   availability_zone = "ap-south-1b"
     tags = {
         Name = "public-subnet-2"
@@ -22,7 +22,7 @@ resource "aws_subnet" "public_2" {
 }
 resource "aws_subnet" "private_1" {
     vpc_id            = aws_vpc.main.id
-    cidr_block        = "172.10.3.0/24"
+    cidr_block        = "172.1.3.0/24"
     availability_zone = "ap-south-1a"
     tags = {
         Name = "private-subnet-1"
@@ -31,7 +31,7 @@ resource "aws_subnet" "private_1" {
 }
 resource "aws_subnet" "private_2" {
     vpc_id            = aws_vpc.main.id
-    cidr_block        = "172.10.4.0/24"
+    cidr_block        = "172.1.4.0/24"
     availability_zone = "ap-south-1b"   
     tags = {
         Name = "private-subnet-2"
@@ -99,3 +99,4 @@ resource "aws_route_table_association" "private_2_assoc" {
     route_table_id = aws_route_table.private_rt.id
   
 }
+
